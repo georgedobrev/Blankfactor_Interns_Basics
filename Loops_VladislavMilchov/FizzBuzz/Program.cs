65108ccde6dc01e172d15d28e54ce751215a714e
VladislavMilchov <vladislav.milchov@blankfactor.com>
2023-04-27 12:37:23 +0300

    Loops tasks

---
+++ Loops_VladislavMilchov/FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int n = 15;
+
+List<string> listFuzzBuzz = new List<string>();
+
+for (int i = 1; i <= n; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+
+		listFuzzBuzz.Add("FizzBuzz");
+
+	}
+
+	else if (i % 3 == 0)
+	{
+        listFuzzBuzz.Add("Fizz");
+	}
+
+    else if (i % 5 == 0)
+    {
+        listFuzzBuzz.Add("Buzz");
+    }
+
+	else
+	{
+		string num = i.ToString();
+        listFuzzBuzz.Add(num);
+    }
+
+	
+	
+};
+
+for (int i = 0; i < listFuzzBuzz.Count; i++)
+{
+	Console.Write(listFuzzBuzz[i] + ", ");
+}
---
+++ Loops_VladislavMilchov/loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Blankfactor_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "abc";
+            string word2 = "pqr";
+
+            for (int i = 0; i < word1.Length; i++)
+            {
+                Console.Write(string.Concat(word1[i]));
+
+                Console.Write(string.Concat(word2[i]));
+            }
+        }
+    }
+}
