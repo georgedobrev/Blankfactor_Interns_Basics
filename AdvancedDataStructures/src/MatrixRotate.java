39e8019278e353f3347309b9022bb91f1d4cce5c
prodanov33 <17422@uktc-bg>
2023-04-28 14:26:05 +0300

    .

--- AdvancedDataStructures/src/MatrixRotate.java
+++ AdvancedDataStructures/src/MatrixRotate.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class MatrixRotate {
     public static void main(String[] args) {
-        int[][] a = {{3, 2, 1},
-                {9, 11, 5},
-                {6, 0, 13}};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of rows of the matrix");
+        int  r = input.nextInt();
+        System.out.println("Enter the number of columns of the matrix");
+        int c = input.nextInt();
 
-        int n = 3;
+        int a[][] = new int[r][c];
+
+        System.out.println("Enter the elements of the matrix");
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                a[i][j] = input.nextInt();
+
+
+
+        int n = r;
         for (int i = 0; i < n / 2; i++) {
             for (int j = i; j < n - i - 1; j++) {
 
--- AdvancedDataStructures/src/PairsOfNumbers.java
+++ AdvancedDataStructures/src/PairsOfNumbers.java
@@ -2,15 +2,20 @@ import java.util.HashMap;
 
 public class PairsOfNumbers {
     public static void main(String[] args) {
+
         Integer[] values = {4, 5, 6, 8, 1};
         int check = 12;
-
         HashMap<Integer, Integer> values2 = new HashMap<Integer, Integer>();
+
+        pairsOfNumbers(values,values2,check);
+
+    }
+
+    private static void pairsOfNumbers(Integer[] values, HashMap<Integer,Integer> values2, int check) {
 
 
         for (int i = 0; i < values.length; i++)
             values2.put(i, values[i]);
-
 
         for (int i = 0; i < values2.size(); i++) {
             int temp = check - values2.get(i);
@@ -22,5 +27,6 @@ public class PairsOfNumbers {
             }
 
         }
+
     }
 }
