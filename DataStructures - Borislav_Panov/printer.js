f707ad34811505a50ec7e685778b6f51fa4e1a14
borislavpanov-blankfactor <borislav.panov@blankfactor.com>
2023-05-02 12:02:15 +0300

    Update printer.js

--- DataStructures - Borislav_Panov/printer.js
+++ DataStructures - Borislav_Panov/printer.js
@@ -1,17 +1,31 @@
 const unableToComplete = (tasks, papers) => {
     const unfinishedTasks = [];
-
     for (let i = tasks.length - 1; i >= 0; i--) {
         const task = tasks[i];
         if (task === papers[i]) {
             tasks.splice(i, 1);
+            papers.splice(i, 1)
         } else {
-            unfinishedTasks.push(task);
+            unfinishedTasks.unshift(task);
         }
     }
+    let k = 0;
+    while (unfinishedTasks.length > 0) {
+        if (unfinishedTasks.includes(papers[k])) {
+            const index = unfinishedTasks.indexOf(papers[k]);
+            unfinishedTasks.splice(index, 1);
+        }
+        k++;
+        if (k >= papers.length) {
+            break;
+        }
+    }
+
     return unfinishedTasks;
 };
 
-
-
+console.log(unableToComplete([1,1,0,0], [0,1,0,1]))
 console.log(unableToComplete([0,0,0,1], [0,0,0,1]))
+console.log(unableToComplete([1,1,1,1], [0,0,0,1]))
+console.log(unableToComplete([1,1,0,0], [0,0,0,1]))
+console.log(unableToComplete([0,1,1,1], [1,1,1,1]))
