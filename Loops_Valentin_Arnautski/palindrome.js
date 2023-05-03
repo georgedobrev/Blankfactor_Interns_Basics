46bd00e2d012e3f3ec66b92c49c114c24d082814
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-05-03 09:08:40 +0300

    a

--- Loops_Valentin_Arnautski/palindrome.js
+++
@@ -1,8 +0,0 @@
-function palindrome(num) {
-    let isPalindrome = false;
-    const reversedNumber = num.toString().split('').reverse().join('');
-  
-    return num.toString() === reversedNumber
-  }
-  
-  palindrome(123321);
