eee52820402ae2cf921ae9e98feff7c2cf264fac
Borislav Panov <borislav.panov@blankfactor.com>
2023-05-03 15:25:36 +0300

    Update printer.js

--- DataStructures - Borislav_Panov/printer.js
+++ DataStructures - Borislav_Panov/printer.js
@@ -1,31 +1,22 @@
-const unableToComplete = (tasks, papers) => {
-    const unfinishedTasks = [];
-    for (let i = tasks.length - 1; i >= 0; i--) {
-        const task = tasks[i];
-        if (task === papers[i]) {
-            tasks.splice(i, 1);
-            papers.splice(i, 1)
+const newPrinter = (tasks, paper) =>  {
+    let br = 0;
+    while (paper.length > 0 && br <= paper.length + 1) {
+        if (paper[paper.length - 1] === tasks[0]) {
+            paper.pop();
+            tasks.shift();
+            br = 0;
         } else {
-            unfinishedTasks.unshift(task);
+            tasks.push(tasks.shift());
+            br++;
         }
     }
-    let k = 0;
-    while (unfinishedTasks.length > 0) {
-        if (unfinishedTasks.includes(papers[k])) {
-            const index = unfinishedTasks.indexOf(papers[k]);
-            unfinishedTasks.splice(index, 1);
-        }
-        k++;
-        if (k >= papers.length) {
-            break;
-        }
-    }
+    return tasks.length;
+}
 
-    return unfinishedTasks;
-};
 
-console.log(unableToComplete([1,1,0,0], [0,1,0,1]))
-console.log(unableToComplete([0,0,0,1], [0,0,0,1]))
-console.log(unableToComplete([1,1,1,1], [0,0,0,1]))
-console.log(unableToComplete([1,1,0,0], [0,0,0,1]))
-console.log(unableToComplete([0,1,1,1], [1,1,1,1]))
+console.log(newPrinter([1,1,0,0], [0,1,0,1]))
+console.log(newPrinter([0,0,0,1], [0,0,0,1]))
+console.log(newPrinter([1,1,1,1], [0,0,0,1]))
+console.log(newPrinter([1,1,0,0], [0,0,0,1]))
+console.log(newPrinter([0,1,1,1], [1,1,1,1]))
+console.log(newPrinter([1,1,1,0,0,1],[1,0,0,0,1,1]))
