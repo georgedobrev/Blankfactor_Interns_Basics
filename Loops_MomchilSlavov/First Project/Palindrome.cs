70e04e1da2c4e4b1cb1dd96f26d3dc6801c6a837
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-05-09 10:40:13 +0300

    Solutions

---
+++ Loops_MomchilSlavov/First Project/Palindrome.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * C# Program aimed at checking whether a certain number is a palindrome or not via a for loop
+ */
+
+public class ForLoop
+{
+
+    public static void main(String[] args)
+    {
+
+        int num = 121;
+
+        int reversedNum = 0, remainder;
+
+
+        int originalNum = num;
+
+        while (num != 0)
+
+        {
+            remainder = num % 10;
+            reversedNum = reversedNum * 10 + remainder;
+            num /= 10;
+        }
+
+        if (originalNum == reversedNum)
+
+        {
+            Console.WriteLine(originalNum + " is a palindrome.");
+        }
+
+        else
+
+        {
+            Console.WriteLine(originalNum + " is not a palindrome.");
+        }
+    }
+}
