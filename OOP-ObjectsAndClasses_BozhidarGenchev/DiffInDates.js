a5c5ed63bc4d7ed3c51cb2ffd6fe682aa4afffb0
kolevgenchevwork <131447168+kolevgenchevwork@users.noreply.github.com>
2023-05-09 16:33:19 +0300

    Update DiffInDates.js
    Changes in the Class constructor.

--- OOP-ObjectsAndClasses_BozhidarGenchev/DiffInDates.js
+++ OOP-ObjectsAndClasses_BozhidarGenchev/DiffInDates.js
@@ -1,15 +1,18 @@
 class DateModifier {
-    constructor(date1,date2){
-      this.date1=new Date(date1)
-      this.date2=new Date(date2)
-    }
-    calculateDiff(){
-      const diffTime = Math.abs(this.date2 - this.date1);
-      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  
-      return diffDays
-    }
+  constructor(date1, date2) {
+    this.date1 = new Date(date1);
+    this.date2 = new Date(date2);
+    this.diffDays = this.calculateDiff();
   }
+
+  calculateDiff() {
+    const diffTime = Math.abs(this.date2 - this.date1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    return diffDays;
+  }
+}
+
+const newDateDiff = new DateModifier('2023-05-01', '2023-05-03');
+console.log(newDateDiff.diffDays);
   
-  let newDateDiff= new DateModifier('2023-05-01', '2023-05-03')
-  console.log(newDateDiff.calculateDiff())
