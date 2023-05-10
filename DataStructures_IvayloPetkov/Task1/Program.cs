50f1dfdfdc38823ba2dedbf9c147d519c51afcb3
Ivaylo Petkov <131447184+IvayloPetkovBlankfactor@users.noreply.github.com>
2023-05-10 10:53:47 +0300

    PascalCase fix

--- DataStructures_IvayloPetkov/Task1/Program.cs
+++ DataStructures_IvayloPetkov/Task1/Program.cs
@@ -5,37 +5,37 @@ namespace Application
     class Program
     {
 
-        static void function()
+        static void Function()
         {
 
             Console.Write("What type of papers are loaded into the printer right now? : plain - 0   or   glossy - 1?");
-            string type_of_paper = Console.ReadLine();
+            string TypeOfPaper = Console.ReadLine();
 
             Console.Write("How many tasks does each user have? ");
-            int num_tasks = int.Parse(Console.ReadLine());
+            int NumTasks = int.Parse(Console.ReadLine());
 
-            string[] users = { "User 1", "User 2", "User 3", "User 4" };
-            string[,] task_types = new string[users.Length, num_tasks];
+            string[] Users = { "User 1", "User 2", "User 3", "User 4" };
+            string[,] TaskTypes = new string[Users.Length, NumTasks];
 
-            for (int i = 0; i < users.Length; i++)
+            for (int i = 0; i < Users.Length; i++)
             {
-                Console.WriteLine($"Enter the type of paper for {users[i]}'s tasks:");
+                Console.WriteLine($"Enter the type of paper for {Users[i]}'s tasks:");
 
-                for (int j = 0; j < num_tasks; j++)
+                for (int j = 0; j < NumTasks; j++)
                 {
                     Console.Write($"Task {j + 1}: ");
-                    task_types[i, j] = Console.ReadLine();
+                    TaskTypes[i, j] = Console.ReadLine();
                 }
             }
 
             Console.WriteLine("The tasks from the following users will be executed:");
-            for (int i = 0; i < users.Length; i++)
+            for (int i = 0; i < Users.Length; i++)
             {
-                for (int j = 0; j < num_tasks; j++)
+                for (int j = 0; j < NumTasks; j++)
                 {
-                    if (task_types[i, j] == type_of_paper)
+                    if (TaskTypes[i, j] == TypeOfPaper)
                     {
-                        Console.WriteLine($"{users[i]} - Task {j + 1}");
+                        Console.WriteLine($"{Users[i]} - Task {j + 1}");
                     }
                 }
             }
