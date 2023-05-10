c47ec761ddaf0228be5f7aa1f8227885e6d4323e
Ivaylo Petkov <131447184+IvayloPetkovBlankfactor@users.noreply.github.com>
2023-05-10 12:15:47 +0300

    Upload Exercise 2

---
+++ Advanced_DataStructure Ivaylo Petkov/Exercise 2/Exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 1, 2, 3, 4, 5 };
+            int Target = 7;
+
+            var Pairs = new System.Collections.Generic.List<(int, int)>();
+            for (int i = 0; i < Array.Length; i++)
+            {
+                for (int j = i + 1; j < Array.Length; j++)
+                {
+                    if (Array[i] + Array[j] == Target)
+                    {
+                        Pairs.Add((Array[i], Array[j]));
+                    }
+                }
+            }
+
+            foreach (var Pair in Pairs)
+            {
+                Console.WriteLine("(" + Pair.Item1 + ", " + Pair.Item2 + ")");
+            }
+        }
+    }
+}
