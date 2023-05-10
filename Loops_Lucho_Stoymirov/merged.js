23f40972886f4524c30741a6f649f6aca1691112
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-05-10 13:24:25 +0300

    Made tasks use functions

--- Loops_Lucho_Stoymirov/merged.js
+++ Loops_Lucho_Stoymirov/merged.js
@@ -1,15 +1,20 @@
-var word1 = "ABC"
-var word2 = "DEF"
-var mixed = '';
-const biggerNum = Math.max(word1.length, word2.length);
 
-for (var i = 0; i < biggerNum; i++) {
-  if (i < word1.length) {
-    mixed += word1.charAt(i);
+let mergeStrings = (x,y) => {
+  var word1 = x;
+  var word2 = y;
+  var mixed = '';
+  const biggerNum = Math.max(word1.length, word2.length);
+
+  for (var i = 0; i < biggerNum; i++) {
+    if (i < word1.length) {
+      mixed += word1.charAt(i);
+    }
+    if (i < word2.length) {
+      mixed += word2.charAt(i);
+    }
   }
-  if (i < word2.length) {
-    mixed += word2.charAt(i);
-  }
+
+console.log(mixed); 
 }
 
-console.log(mixed);
+mergeStrings("ABC", "DEF");
