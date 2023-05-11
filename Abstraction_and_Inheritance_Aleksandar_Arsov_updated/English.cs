08b816a2c71fe3f748fc7f20298fc2867ab86c70
aleksandararsov <aleksandar.arsov@blankfactor.com>
2023-05-11 09:57:52 +0300

    Updated

---
+++ Abstraction_and_Inheritance_Aleksandar_Arsov_updated/English.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Abstraction_and_Inheritance
+{
+	public interface English
+	{
+        string EnglishTranslation { get; set; }
+    }
+}
+
---
+++ Abstraction_and_Inheritance_Aleksandar_Arsov_updated/Verb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Abstraction_and_Inheritance
+{
+	public class Verb : Words
+	{
+        public string Infinitive { get; set; }
+        public override void DisplayWordInfo()
+        {
+            Console.WriteLine(English);
+            Console.WriteLine(Bulgarian);
+            Console.WriteLine(Infinitive);
+        }
+    }
+}
+
