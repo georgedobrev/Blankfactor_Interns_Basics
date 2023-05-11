bb22d9d2570529fe51707c9fd5e69ad7ee0d45db
Radoslav Georgiev <radoslavasz@abv.bg>
2023-05-11 16:59:57 +0300

    Fixed

--- Loops_RadoslavGeorgiev/src/MergeTwoStrings.java
+++ Loops_RadoslavGeorgiev/src/MergeTwoStrings.java
@@ -1,10 +1,17 @@
 public class MergeTwoStrings {
-    private static  void merging(){
-        String word1 = "abc";
-        String word2 = "pqr";
-        for (int i = 0; i < word1.length(); i++) {
-            System.out.print(word1.charAt(i));
-            System.out.print(word2.charAt(i));
+    private static  void merging() {
+      String word1 = "312312";
+      String word2 = "kfsf";
+        if (word1.length() > word2.length()) {
+            for (int i = 0; i < word1.length(); i++) {
+                System.out.print(word1.charAt(i % word1.length()));
+                System.out.print(word2.charAt(i % word2.length()));
+            }
+        } else {
+            for (int i = 0; i < word2.length(); i++) {
+                System.out.print(word1.charAt(i % word1.length()));
+                System.out.print(word2.charAt(i % word2.length()));
+            }
         }
     }
     public static void main(String[] args) {
