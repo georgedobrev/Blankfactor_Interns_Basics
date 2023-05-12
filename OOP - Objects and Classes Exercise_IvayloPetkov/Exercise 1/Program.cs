74a9b1100e09f97f528d5e99519d4a08a9618870
Ivaylo Petkov <131447184+IvayloPetkovBlankfactor@users.noreply.github.com>
2023-05-12 10:27:47 +0300

    Task 1 and 2

---
+++ OOP - Objects and Classes Exercise_IvayloPetkov/Exercise 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Application
+{
+    public class DateModifier
+    {
+        public static int Difference(string FirstDateString, string SecondDateString)
+        {
+            DateTime FirstDateTime = DateTime.ParseExact(FirstDateString, "yyyy MM dd", null);
+            DateTime SecondDateTime = DateTime.ParseExact(SecondDateString, "yyyy MM dd", null);
+            TimeSpan TimeDifference = SecondDateTime - FirstDateTime;
+            return Math.Abs(TimeDifference.Days);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date1 = "2022 05 10";
+            string date2 = "2022 05 12";
+            int days = DateModifier.Difference(date1, date2);
+            Console.WriteLine($"The difference between {date1} and {date2} is {days} days.");
+        }
+    }
+}
