ca5dcfe00a4869fe168e1a5d213cb143cd110d11
Ivaylo Petkov <131447184+IvayloPetkovBlankfactor@users.noreply.github.com>
2023-05-12 12:00:15 +0300

    Fix

---
+++ Loops_IvayloPetkov/Check the sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine();
+            string secondWord = Console.ReadLine();
+            string targetWord = Console.ReadLine();
+
+            int counter_firstWord = 0;
+            int counter_secondWord = 0;
+            int counter_thirdWord = 0;
+            var new_first_word = firstWord.Length;
+            var new_second_word = secondWord.Length;
+            var new_target_word = targetWord.Length;
+
+            for (int i = 0; i < new_first_word; i++)
+            {
+                counter_firstWord += firstWord[i] - 96;
+            }
+
+            for (int i = 0; i < new_second_word; i++)
+            {
+                counter_secondWord += secondWord[i] - 96;
+            }
+
+            for (int i = 0; i < new_target_word; i++)
+            {
+                counter_thirdWord += targetWord[i] - 96;
+            }
+
+            if (counter_firstWord == counter_secondWord && counter_firstWord == counter_thirdWord && counter_secondWord == counter_thirdWord)
+            {
+                Console.WriteLine("True");
+            }
+
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+        }
+    }
+}
---
+++ Loops_IvayloPetkov/Merge two strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = Console.ReadLine();
+            string string2 = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < string1.Length || i < string2.Length; i++)
+            {
+
+                if (i < string1.Length)
+                    result.Append(string1[i]);
+
+                if (i < string2.Length)
+                    result.Append(string2[i]);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
