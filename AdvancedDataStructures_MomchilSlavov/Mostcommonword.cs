b2d2e5ebf6abf28a84220740f26dfb7621a02578
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-05-12 14:30:33 +0300

    Create Mostcommonword.cs

---
+++ AdvancedDataStructures_MomchilSlavov/Mostcommonword.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MostCommonWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string: ");
+
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+
+            Dictionary<string, int> Mostcommonword = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (Mostcommonword.ContainsKey(word))
+                {
+                    Mostcommonword [word]++;
+                }
+                else
+                {
+                    Mostcommonword.Add(word, 1);
+                }
+            }
+
+            int maxVal = Mostcommonword.Values.Max();
+
+            foreach (string key in Mostcommonword.Keys)
+            {
+                if (Mostcommonword[key] == maxVal)
+                {
+                    Console.WriteLine($"\"{key}\" (appears {maxVal} times)");
+                }
+            }
+
+
+        }
+    }
+}
