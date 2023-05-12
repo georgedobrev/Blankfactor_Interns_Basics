5a1c9a688636623ed94a933fdfe259e95d4bd06f
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-05-12 11:03:29 +0300

    First Part

---
+++ Abstraction_MomchilSlavov/InheritedClass1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Abstraction_AndInheritance
+{
+    public class KilometresCompleted : LuxuryCarBrands
+    {
+
+        public string NewYork { get; set; }
+        public override void DisplayLuxuryCarInfo()
+
+        {
+            Console.WriteLine (Dubai);
+            Console.WriteLine (London);
+            Console.WriteLine (NewYork);
+        }
+    }
+}
+
---
+++ Abstraction_MomchilSlavov/InheritedClass2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Abstraction_AndInheritance
+{
+    public class CarPrice : LuxuryCarBrands
+    {
+
+        public string NewYork { get; set; }
+        public override void DisplayLuxuryCarInfo()
+
+        {
+            Console.WriteLine(Dubai);
+            Console.WriteLine(London);
+            Console.WriteLine(NewYork);
+        }
+    }
+}
+
