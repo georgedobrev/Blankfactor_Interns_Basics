9e4fde4da1d15f44cdd2a37fac5dbe4b80af013f
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-05-12 11:36:36 +0300

    Create FizzBuzz.cs

---
+++ Loops_MomchilSlavov/First Project/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * C# Program aimed at displaying "Fizz", "Buzz" or "FizzBuzz" based on the division of a certain number
+ */
+
+public class For_Loop
+{
+    public static void main(String[] args)
+    {
+        int n = 15;
+        for (int i = 1; i <= 15; i++)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            //System.out.println(i);
+            else if (n % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (n % 5 == 0)
+                Console.WriteLine("Buzz");
+        }
+    }
+}
+
+
+   
+
+
+
+
+
+    
