c87ae6d0bba49291768442f687cfadc03788c24d
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-05-15 10:33:53 +0300

    new

---
+++ App_momchil/Interface1.cs
@@ -0,0 +1,30 @@
+﻿// Interface exercise
+
+using Abstraction_AndInheritance;
+
+
+interface Colour
+{
+    void CarColour ();
+}
+
+// Porsche "implements" the LuxuryCarColour interface
+
+class Porsche : LuxuryCarColour
+{
+    public void CarColour()
+    {
+
+        Console.WriteLine("The colour of the luxury car described above is: red ");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Porsche myPorsche = new Porsche();
+
+        myPorsche.CarColour();
+    }
+}
