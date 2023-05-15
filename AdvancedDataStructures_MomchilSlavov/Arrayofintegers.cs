44307ed0c75809b01d38634732b872e19c0cf276
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-05-15 08:44:41 +0300

    Additional

---
+++ AdvancedDataStructures_MomchilSlavov/Arrayofintegers.cs
@@ -0,0 +1,25 @@
+﻿
+class Arrayofintegers
+{
+    static void Main(string[] args)
+    {
+        int target;
+        target = Int.Parse(Console.ReadLine());
+        Console.WriteLine("Input the desired integer number: ");
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        Dictionary<int> dictionary = new Dictionary<int>();
+
+        foreach (int number in numbers)
+        {
+            int difference = target - number;
+
+            if (set.Contains(difference))
+            {
+                Console.WriteLine($"The {difference}, is {number}");
+            }
+            set.Add(number);
+        }
+    }
+}
---
+++ AdvancedDataStructures_MomchilSlavov/Matrixrotation.cs
@@ -0,0 +1,51 @@
+﻿
+class Matrixrotation
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Square matrix rows(cols) = ");
+        int n = int.Parse(Console.ReadLine());
+
+
+        int[,] matrix = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[j, i];
+                matrix[j, i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n / 2; ++j)
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[i, n - j - 1];
+                matrix[i, n - j - 1] = temp;
+            }
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(string.Format("{0} ", matrix[i, j]));
+            }
+            Console.Write(Environment.NewLine + Environment.NewLine);
+        }
+
+    }
+}
