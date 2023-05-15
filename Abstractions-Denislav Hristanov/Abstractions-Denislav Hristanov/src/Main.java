8a10593b4a3064b96435a2afe22e764737c184d2
DenislavHristanov <131447322+DenislavHristanov@users.noreply.github.com>
2023-05-15 15:00:25 +0300

    Abstractions-DenislavHristanov (#152)
    * Abstractions-DenislavHristanov
    
    * Add files via upload
    
    * Delete Animal.java

---
+++ Abstractions-Denislav Hristanov/Abstractions-Denislav Hristanov/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[]args) {
+        Animal animal = new Dog("Buddy",5);
+        System.out.println("Dog's name: " + animal.name);
+        System.out.println("Dog's age: " + animal.age);
+        animal.makeSound();
+
+        System.out.println();
+
+        Pet pet = new Cat("Mittens",3);
+        System.out.println("Cats name: " +(Animal)pet).name);
+
+        System.out.println((Cat's));
+    }
+}
